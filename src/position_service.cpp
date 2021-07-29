40ab43fb62a7b3ec9fbc0ff92ead238341e4c271
LitongHuang <79423398+LitongHuang@users.noreply.github.com>
2021-07-29 18:19:53 +0200

    Add files via upload

--- src/position_service.cpp
+++ src/position_service.cpp
@@ -1,22 +1,42 @@
 #include "ros/ros.h"
-#include "rt2_ass1/RandomPosition.h"
+#include "rt2_ass2/RandomPosition.h"
 
-// funtion to obtain a random number between minimum and maximum values for x and y
+// Description: 
+//    This node implements the server that returns a random position and 
+//	 orientation for the robot between given intervals. 
+
+/**
+ * \brief: It generates a random number
+ * \param M: the lower bound of the interval I want to have the random number in
+ * \param N: the upper bound of the interval I want to have the random number in
+ * 
+ * \return: the random number generated
+ * 
+ * This function uses the library function rand() to generate a random number and
+ *  then resizes it to be in the interval [M;N].
+ */
 double randMToN(double M, double N)
 {     return M + (rand() / ( RAND_MAX / (N-M) ) ) ; }
 
-// when the request from the client exixts, the function called 
-bool myrandom (rt2_ass1::RandomPosition::Request &req, rt2_ass1::RandomPosition::Response &res){
+/**
+ * This function calls the function randMtoN to generate three random numbers,
+ * the x coordinate, the y coordinate and the orientation that is said to be
+ * requested between -pi and pi.
+ */
+bool myrandom (rt2_ass2::RandomPosition::Request &req, rt2_ass2::RandomPosition::Response &res){
     res.x = randMToN(req.x_min, req.x_max);
     res.y = randMToN(req.y_min, req.y_max);
     res.theta = randMToN(-3.14, 3.14);
     return true;
 }
 
-
+/**
+ * This function initializes the ros node and the server /position_server,
+ * it then runs waiting for a request for the server.
+ */
 int main(int argc, char **argv)
 {
-   // initialization of the node 'position server'
+	// initialization of the node
    ros::init(argc, argv, "random_position_server");
    ros::NodeHandle n;
    // initialization of the server
